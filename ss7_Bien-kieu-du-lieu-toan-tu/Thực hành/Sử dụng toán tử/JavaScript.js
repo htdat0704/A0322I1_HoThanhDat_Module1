eb4db954e5a36fd270b3f9085b39cdc2f06858fe
htdat0704 <htdat.18it3@sict.udn.vn>
2022-04-08 18:22:52 +0700

    Update TH

---
+++ ss7_Bien-kieu-du-lieu-toan-tu/Thực hành/Sử dụng toán tử/JavaScript.js
@@ -0,0 +1,11 @@
+let inputWidth ;
+let inputHeight ;
+
+inputWidth = prompt("Nhập chiều rộng = ");
+inputHeight = prompt("Nhập chiều dài = ");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("Area = " +area);
